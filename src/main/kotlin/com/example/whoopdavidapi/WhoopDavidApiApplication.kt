67efe616c11fc747c00cd8c51e2e30990cc35d2d
PabloHurtadoGonzalo <pablofullstackdevelopernrby@gmail.com>
2026-02-07 00:02:43 +0100

    first commit

---
+++ src/main/kotlin/com/example/whoopdavidapi/WhoopDavidApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.whoopdavidapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhoopDavidApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<WhoopDavidApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/whoopdavidapi/WhoopDavidApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.whoopdavidapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhoopDavidApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
